ca170f560670a704b1fa5fb9237f96f08b42040a
Arexon <arexonreal@gmail.com>
2021-08-24 02:47:42 +0200

    ✨ Refactor Sittable

--- BP/components/block/Sittable.js
+++ BP/components/block/Sittable.js
@@ -1,74 +1,68 @@
 export default defineComponent(({ name, template, schema }) => {
 	name('furnideco:sittable')
 	schema({
-		'directional': 'boolean'
+		directional: 'boolean'
 	})
 
 	template(({ directional = false }, { create, identifier }) => {
 
 		create(
 			{
-				"minecraft:on_interact": {
-					"condition": "q.get_equipped_item_name != 'paint_brush' && !q.is_sneaking",
-					"event": "e:add.rider"
+				'minecraft:on_interact': {
+					condition: `q.get_equipped_item_name != 'paint_brush' && !q.is_sneaking`,
+					event: 'e:add.rider'
 				},
-				"minecraft:on_placed": {
-					"event": "e:add.seat"
+				'minecraft:on_placed': {
+					event: 'e:add.seat'
 				}
 			},
-			"minecraft:block/components"
+			'minecraft:block/components'
 		)
 
-		const directions = new Map([
-			[ 1, 'e:set.direction.east' ],
-			[ 2, 'e:set.direction.south' ],
-			[ 3, 'e:set.direction.west' ]
-		])
-
-		for (let i = 0; i < 4; i++) {
-			const seat = {
-				"run_command": {
-					"command": "summon furnideco:seat." + identifier.split(/[\.\:]/)[1] + "_" + identifier.split(/[\.\:]/)[3] + " ~~~ " + (i > 0 && directional ? directions.get(i) : '')
-				}
-			}
-
-			const directionalSeat = []
-			directionalSeat['sequence'] = [
-				{
-					"condition": "q.block_property('p:direction') == " + i,
-					...seat
-				}
-			]
-
-			create(
-				{
-					"e:add.seat": {
-						...(directional ? directionalSeat : seat)
+		const directions = [
+			'e:set.direction.east',
+			'e:set.direction.south',
+			'e:set.direction.west'
+		]
+		create(
+			{
+				'e:add.seat': (
+					directional ? {
+						sequence: directions.map((direction, i) => ({
+							condition: `q.block_property('p:direction') == ${i}`,
+							run_command: {
+								command: `summon furnideco:seat.${identifier.split(/[\.\:]/)[1]}_${identifier.split(/[\.\:]/)[3]} ~~~ ${direction}`
+							}
+						}))
+					} : {
+						run_command: {
+							command: `summon furnideco:seat.${identifier.split(/[\.\:]/)[1]}_${identifier.split(/[\.\:]/)[3]} ~~~`
+						}
 					}
-				},
-				"minecraft:block/events"
-			)
-		}
+				)
+			},
+			'minecraft:block/events'
+		)
 
 		create(
 			{
-				"e:add.rider": {
-					"sequence": [
+				'e:add.rider': {
+					sequence: [
 						{
-							"run_command": {
-								"command": "tag @s add pre_sitting",
-								"target": "other"
+							run_command: {
+								command: 'tag @s add pre_sitting',
+								target: 'other'
 							}
 						},
 						{
-							"run_command": {
-								"command": "event entity @e[family=seat, r=0.1] e:add.rider"
+							run_command: {
+								command: 'event entity @e[family=seat, r=0.1] e:add.rider'
 							}
 						}
 					]
 				}
 			},
-			"minecraft:block/events"
+			'minecraft:block/events'
 		)
 	})
 })
