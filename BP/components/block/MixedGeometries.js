1409f99075ebd0b5759a6168e7e187147e343711
Arexon <arexonreal@gmail.com>
2021-08-26 21:31:28 +0200

    🔧 Change the way mixed geometries are handled

--- BP/components/block/MixedGeometries.js
+++ BP/components/block/MixedGeometries.js
@@ -2,17 +2,17 @@ export default defineComponent(({ name, template, schema }) => {
 	name('furnideco:mixed_geometries')
 	schema({
 		geometries: 'array',
-		key: 'string',
+		property: 'string',
 		loot_table: 'boolean'
 	})
 
-	template(({ geometries = [], key = '', loot_table = false }, { create, identifier }) => {
+	template(({ geometries = [], property = 'p:geometry', loot_table = false }, { create, identifier }) => {
 
 		let geosCount = []
 		for (let i = 0; i < geometries.length; i++) geosCount.push(i)
 		create(
 			{
-				'p:geometry': geosCount
+				[property]: geosCount
 			},
 			'minecraft:block/description/properties'
 		)
@@ -20,20 +20,20 @@ export default defineComponent(({ name, template, schema }) => {
 		create(
 			{
 				permutations: geometries.map((geo, i) => ({
-					condition: `q.block_property('p:geometry') == ${i}`,
+					condition: `q.block_property('${property}') == ${i}`,
 					components: {
-						'minecraft:geometry': `geometry.${identifier.split(/[\.\:]/)[1]}.${identifier.split('.')[1]}.${key}_${i}`,
-						'minecraft:pick_collision': (geo.pick_collision == false ? false : {
-							origin: geo.pick_collision.slice(0, 3),
-							size: geo.pick_collision.slice(3, 6)
-						}),
-						'minecraft:entity_collision': (geo.entity_collision == false ? false : {
-							origin: geo.entity_collision.slice(0, 3),
-							size: geo.entity_collision.slice(3, 6)
+						'minecraft:geometry': `geometry.${identifier.split(/[\.\:]/)[1]}.${identifier.split('.')[1]}.${geo.name}`,
+						'minecraft:pick_collision': {
+							origin: geo.hitbox.pick.slice(0, 3),
+							size: geo.hitbox.pick.slice(3, 6)
+						},
+						'minecraft:entity_collision': (geo.hitbox.entity == false ? false : {
+							origin: geo.hitbox.entity.slice(0, 3),
+							size: geo.hitbox.entity.slice(3, 6)
 						}),
 						...(loot_table && {
-							'minecraft:loot': `loot_tables/${loot_table}.${key}_${i}.loot.json`
-						}),
+							'minecraft:loot': `loot_tables/${loot_table}/${geo.name}.loot.json`
+						})
 					}
 				}))
 			},
