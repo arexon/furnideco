261817c50991ddfa786501c36881cfaa443509aa
Arexon <arexonreal@gmail.com>
2021-08-27 20:32:11 +0200

    🔧 Create basic boiler plate for Connectable

--- BP/components/block/Connectable.js
+++ BP/components/block/Connectable.js
@@ -1,8 +1,21 @@
 export default defineComponent(({ name, template, schema }) => {
 	name('furnideco:connectable')
-	schema({})
+	schema({
+		tag: 'string',
+		keys: 'array'
+	})
 
-	template(({ }, { create, identifier }) => {
+	template(({ tag = '', keys = [] }, { create, identifier }) => {
 
+		let neighbors = (north, east, south, west) => {
+			let property = 'q.block_neighbor_has_any_tag'
+			let properties = [
+				[ `${north ? '' : '!'}${property}(0, 0, -1, '${tag}')` ],
+				[ `${east ? '' : '!'}${property}(1, 0, 0, '${tag}')` ],
+				[ `${south ? '' : '!'}${property}(0, 0, 1, '${tag}')` ],
+				[ `${west ? '' : '!'}${property}(-1, 0, 0, '${tag}')` ]
+			]
+			return `${properties[0]} && ${properties[1]} && ${properties[2]} && ${properties[3]}`
+		}
 	})
 })
