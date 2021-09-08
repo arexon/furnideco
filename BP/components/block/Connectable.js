8dfd91612ab0fbf4cf38325504cdebc791c03f81
Arexon <arexonreal@gmail.com>
2021-09-08 14:25:52 +0200

    🚧 Tweaks to Connectable

--- BP/components/block/Connectable.js
+++ BP/components/block/Connectable.js
@@ -1,19 +1,17 @@
 export default defineComponent(({ name, template, schema }) => {
 	name('furnideco:connectable')
 	schema({
-		tag: 'string',
-		keys: 'array'
+		tag: 'string'
 	})
 
-	template(({ tag = '', keys = [] }, { create, identifier }) => {
+	template(({ tag = '' }, { }) => {
 
-		let neighbors = (north, east, south, west) => {
-			let property = 'q.block_neighbor_has_any_tag'
-			let properties = [
-				[ `${north ? '' : '!'}${property}(0, 0, -1, '${tag}')` ],
-				[ `${east ? '' : '!'}${property}(1, 0, 0, '${tag}')` ],
-				[ `${south ? '' : '!'}${property}(0, 0, 1, '${tag}')` ],
-				[ `${west ? '' : '!'}${property}(-1, 0, 0, '${tag}')` ]
+		const createNeighbors = (north, east, south, west) => {
+			const properties = [
+				[ `${north ? '' : '!'}q.block_neighbor_has_any_tag(0, 0, -1, '${tag}')` ],
+				[ `${east ? '' : '!'}q.block_neighbor_has_any_tag(1, 0, 0, '${tag}')` ],
+				[ `${south ? '' : '!'}q.block_neighbor_has_any_tag(0, 0, 1, '${tag}')` ],
+				[ `${west ? '' : '!'}q.block_neighbor_has_any_tag(-1, 0, 0, '${tag}')` ]
 			]
 			return `${properties[0]} && ${properties[1]} && ${properties[2]} && ${properties[3]}`
 		}
