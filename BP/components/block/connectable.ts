00428ed817c93e397deb22f3829ac20666861790
Arexon <arexonreal@gmail.com>
2021-10-02 21:18:55 +0200

    ✨ Add support for "!" operator in connectable

--- BP/components/block/connectable.ts
+++ BP/components/block/connectable.ts
@@ -29,7 +29,12 @@ export default defineComponent(({ name, template, schema }) => {
 						directions: {
 							description: 'Specifies when to show the part. Multiple directions can be passed.',
 							type: 'array',
-							items: { enum: [ 'north', 'east', 'south', 'west', 'up', 'down' ] }
+							items: {
+								enum: [
+									'north', 'east', 'south', 'west', 'up', 'down',
+									'!north', '!east', '!south', '!west', '!up', '!down'
+								]
+							}
 						}
 					}
 				}
@@ -45,9 +50,14 @@ export default defineComponent(({ name, template, schema }) => {
 							type: 'string'
 						},
 						directions: {
-							description: 'When to show the geometry',
+							description: 'Specifies when to show the geometry. Multiple directions can be passed.',
 							type: 'array',
-							items: { enum: [ 'north', 'east', 'south', 'west', 'up', 'down' ] }
+							items: {
+								enum: [
+									'north', 'east', 'south', 'west', 'up', 'down',
+									'!north', '!east', '!south', '!west', '!up', '!down'
+								]
+							}
 						}
 					}
 				}
@@ -65,6 +75,7 @@ export default defineComponent(({ name, template, schema }) => {
 			[ 'up', [ 0, 1, 0 ] ],
 			[ 'down', [ 0, -1, 0 ] ]
 		])
+		const createNeighborProperty = (dir: string) => `q.block_property('p:${dir}_neighbor')`
 
 		// Maps through directions and creates a property for each direction
 		directions.map((dir: string) => {
@@ -82,9 +93,9 @@ export default defineComponent(({ name, template, schema }) => {
 				create(
 					{
 						...(part.directions.length === 1 ? {
-							[part.name]: `q.block_property('bridge:${part.directions}_neighbor')`
+							[part.name]: part.directions.toString().charAt(0) === '!' ? `!${createNeighborProperty(part.directions.toString().substring(1))}` : createNeighborProperty(part.directions)
 						} : {
-							[part.name]: `${part.directions.map((dir: string) => `q.block_property('bridge:${dir}_neighbor')`).join('&&')}`
+							[part.name]: `${part.directions.map((dir: string) => dir.charAt(0) === '!' ? `!${createNeighborProperty(dir.substring(1))}` : createNeighborProperty(dir)).join('&&')}`
 						})
 					},
 					'minecraft:block/components/minecraft:part_visibility/rules'
@@ -98,9 +109,9 @@ export default defineComponent(({ name, template, schema }) => {
 				{
 					permutations: geometries.map(geo => ({
 						...(geo.directions.length === 1 ? {
-							condition: `q.block_property('p:${geo.directions}_neighbor')`
+							condition: geo.directions.toString().charAt(0) === '!' ? `!${createNeighborProperty(geo.directions.toString().substring(1))}` : createNeighborProperty(geo.directions)
 						} : {
-							condition: `${geo.directions.map((dir: string) => `q.block_property('p:${dir}_neighbor')`).join('&&')}`
+							condition: `${geo.directions.map((dir: string) => dir.charAt(0) === '!' ? `!${createNeighborProperty(dir.substring(1))}` : createNeighborProperty(dir)).join('&&')}`
 						}),
 						components: {
 							'minecraft:geometry': `geometry.${geo.name}`
